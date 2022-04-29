bb8f73ceb27fe776fdc839a19592c1f584749d8c
ltjerm <ltjerm@yahoo.com>
2022-04-29 10:52:03 -0600

    p2 update

--- controllers/habitsController.js
+++ controllers/habitsController.js
@@ -2,26 +2,23 @@ const express = require('express')
 const Habit = require('../models/habits-model')
 const router = express.Router()
 
-// router.get('/', (req, res) => {
-//   Habit
-//   .find({}).then((habits) => res.render('habits'))
-// })
+router.get('/', (req, res) => {
+  res.render('index')
+})
 
-router.get('/', async (req, res, next) => {
-  try {
-      const habits = await Habit.find({});
-      const context = { habits }
-      console.log(context);
-      return res.render('index.ejs', context);
-  } catch (error) {
-      console.log(error);
-      req.error = error;
-      return next();
-  }
-});
+
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.get('/habits', (req, res) => {
+  Habit
+  .find({}).then((habits) => res.render('habits', {display: habits}))
+})
 
 router.post('/add', (req, res) => {
-  Habit.create(req.body).then((habits) => res.send(habits))
+  console.log(req)
+  Habit.create(req.body).then((habits) => res.redirect('/habits'))
   .catch(console.error)
 })
 
