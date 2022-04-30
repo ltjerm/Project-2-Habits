1f451c6212a2bce72121f445062c8429d939fb90
ltjerm <ltjerm@yahoo.com>
2022-04-30 08:40:15 -0600

    ready for p2 upload

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const habitController = require('./controllers/habitsController')
+const methodOverride = require('method-override')
 app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname + '/' + 'public'))
+
+app.use(methodOverride("_method"))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 3000);
 
-app.use("/", habitController)
+app.use(habitController)
+
 
 app.listen(app.get('port'), () => {
   console.log(`App listening on port ${app.get('port')}`);
